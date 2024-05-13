fcd3949f67d1e3fa9907ee5198949f4e3c0a337b
Matthew Meppelink <20mmeppelink@gmail.com>
2024-05-12 21:34:52 -0500

    added .log to .gitignore, and removed the guild to discordmessage relationship

--- Database/Tables/DiscordMessages.sql
+++ Database/Tables/DiscordMessages.sql
@@ -9,6 +9,5 @@ CREATE TABLE DiscordMessages (
     is_bot BIT NOT NULL,
     attachment_url NVARCHAR(MAX),
     CONSTRAINT FK_DiscordMessage_Channel FOREIGN KEY (channel_id) REFERENCES Channels(channel_id),
-    CONSTRAINT FK_DiscordMessage_Guild FOREIGN KEY (guild_id) REFERENCES Guilds(guild_id),
     CONSTRAINT FK_DiscordMessage_Author FOREIGN KEY (author_id) REFERENCES Users(user_id)
 );
