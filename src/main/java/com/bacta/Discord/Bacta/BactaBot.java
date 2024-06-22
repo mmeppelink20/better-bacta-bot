e34da823738fc43b48f347821c8fff88ca06dba9
Matthew Meppelink <20mmeppelink@gmail.com>
2024-06-22 12:56:30 -0500

    removed unnecssary code

--- src/main/java/com/bacta/Discord/Bacta/BactaBot.java
+++ src/main/java/com/bacta/Discord/Bacta/BactaBot.java
@@ -2,8 +2,6 @@ package com.bacta.Discord.Bacta;
 
 import java.util.ArrayList;
 
-import org.jetbrains.annotations.NotNull;
-
 import com.bacta.Discord.Bacta.EventHandlers.ButtonEventHandler;
 import com.bacta.Discord.Bacta.EventHandlers.MessageRecievedEventHandler;
 import com.bacta.Discord.Bacta.EventHandlers.SlashCommandInteractionEventHandler;
@@ -13,9 +11,6 @@ import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.channel.Channel;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
@@ -43,28 +38,4 @@ public class BactaBot extends ListenerAdapter {
         devIDList.add("197944571844362240");
     }
 
-    // Sends a message to the channel
-    public static void sendAMessage(String message, Guild guild, Channel channel) {
-        guild.getTextChannelById(channel.getId()).sendMessage(message).queue();
-    }
-
-    // Sends a message to the channel, with event object for logging.
-    public void sendAMessage(@NotNull SlashCommandInteractionEvent event, String message, Guild guild, Channel channel) {
-        guild.getTextChannelById(channel.getId()).sendMessage(message).queue();
-        System.out.println("DEBUG: \n" + event.getUser() + " sent: " + event.getOption("message").getAsString());
-    }
-
-    // Sends a sticker to the channel
-    // @TODO
-    public void sendASticker(String sticker, Guild guild, Channel channel) {
-        guild.getTextChannelById(channel.getId()).sendMessage(sticker).queue();
-    }
-
-    // @TODO
-    // Write a method to send a message to every channel in the guild list
-    public void sendToAllChannels(String message, Guild guild) {
-        for (String channelID : guildMessageList.getGuildChannelMap().get(guild.getId())) {
-            sendAMessage(message, guild, guild.getTextChannelById(channelID));
-        }
-    }
 }
